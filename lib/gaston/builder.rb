03e0c96cbaba739a3c9720810e7e6ed4346f00fd
chatgris <jboyer@af83.com>
2013-01-23 21:01:17 +0100

    Code cleanup
    Signed-off-by: chatgris <jboyer@af83.com>

--- lib/gaston/builder.rb
+++ lib/gaston/builder.rb
@@ -4,20 +4,20 @@ class Gaston
     class << self
       def new(parent, hash={})
         hash.each_with_object({}) do |(key, store), hsh|
-          camelize = Inflecto.camelize(key)
-          begin
-            klass = Inflecto.constantize("#{parent}::#{camelize}")
-          rescue NameError
-            klass = parent.const_set Inflecto.camelize(key), Class.new(Hash)
+          if store.is_a?(Hash)
+            camelize = Inflecto.camelize(key)
+            klass = begin
+                      Inflecto.constantize("#{parent}::#{camelize}")
+                    rescue NameError
+                      parent.const_set camelize, Class.new(Hash)
+                    end
+            store = klass[Gaston::Builder.new(klass, store)]
           end
-          store = klass[Gaston::Builder.new(klass, store)] if store.is_a?(Hash)
           if parent.instance_methods.include? key.to_sym
             warn "#{key} method already exists on #{parent}, value: #{store}"
           else
-            store.tap {|s|
-              hsh[key] = s
-              parent.send(:define_method, key, -> { s })
-            }
+            hsh[key] = store
+            parent.send(:define_method, key, -> { store })
           end
         end
       end
